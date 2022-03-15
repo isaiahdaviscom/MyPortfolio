b5586285f755dbb6e9d2fe836cc8e94b5328337c
Isaiah Davis <isaiah.davisx@gmail.com>
2022-03-15 17:50:56 -0500

    update manifest file

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import style from './style.module.css';
 
 const Header = () => (
 	<header className={style.header}>
-		{/* <a href="/"><h1>Jane Doe</h1></a> */}
+		<a href="/"><h1>Jane Doe</h1></a>
 		{/* <nav>
 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
 			<Link activeClassName={style.active} href="/podcasts">Podcasts</Link>

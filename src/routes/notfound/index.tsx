7e9f2aeff57fc9ef5ceec16e632fa509eb800711
Isaiah Davis <isaiah.davisx@gmail.com>
2024-03-17 21:17:45 -0500

    resolved props and dependency flags

--- src/routes/notfound/index.tsx
+++ src/routes/notfound/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 import style from './style.module.css';
 import { Link } from "react-router-dom";
 
 const Notfound = () => {
 
     return (
-        <div class={style.notfound}>
+        <div className={style.notfound}>
             <h1>Error 404</h1>
             <p>That page doesn't exist.</p>
-            <Link href="/"><h4>Back to Home</h4></Link>
+            <Link to="/"><h4>Back to Home</h4></Link>
         </div>
     )
 }
